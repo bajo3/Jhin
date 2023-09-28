f6e20ef9a50b5f1edfa94b3e25904fa9e8dcc993
Felipe Lentini <99766298+bajo3@users.noreply.github.com>
2023-09-28 00:20:15 -0300

    video

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,18 @@ const menuToggle = document.querySelector('.menu-toggle');
 const menuList = document.querySelector('.menu-list');
 const menuItems = document.querySelectorAll('.menu-list li a');
 
+const entrarBtn = document.getElementById('entrar-btn');
+    const videoContainer = document.getElementById('video-container');
+    const contenidoPrincipal = document.getElementById('contenido-principal');
+
+    entrarBtn.addEventListener('click', function () {
+        // Oculta el video y el botón
+        videoContainer.style.display = 'none';
+
+        // Muestra el contenido principal
+        contenidoPrincipal.classList.remove('hidden');
+    });
+
 menuToggle.addEventListener('click', () => {
     menuList.classList.toggle('active');
 });
@@ -10,4 +22,6 @@ menuItems.forEach(item => {
     item.addEventListener('click', () => {
         menuList.classList.remove('active');
     });
+
+    
 });
